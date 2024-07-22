7ed391016f457025428c75231740020d69a3a280
AravindKumar40m <aravindkumar40m@gmail.com>
2024-07-22 14:58:27 +0530

    userDetail routes and api added

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const typesRouter = require("./src/routes/typesRouter");
 const itemsRouter = require("./src/routes/itemsRouter");
 const clientRouter = require("./src/routes/clientRouter");
 const calendarRouter = require("./src/routes/calendarRouter");
-const eventRouter  = require("./src/routes/eventRouter")
+const eventRouter = require("./src/routes/eventRouter");
+const userDetailRouter = require("./src/routes/userDetailRouter");
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
@@ -55,6 +56,7 @@ app.use("/api/v1/items", itemsRouter);
 app.use("/api/v1/events", eventRouter);
 app.use("/api/v1/", clientRouter);
 app.use("/api/v1/", calendarRouter);
+app.use("/api/v1/", userDetailRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
