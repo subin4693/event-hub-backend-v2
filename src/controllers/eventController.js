0287c5a602f49d931204fb5e9592dcac7ed69f9d
subin4693 <46subin@gmail.com>
2024-07-23 15:39:36 +0530

    events filtering completed for users

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -3,7 +3,7 @@ const Booking = require("../models/bookingModel");
 
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const { GridFSBucket } = require("mongodb");
 const Item = require("../models/itemsModel");
 
@@ -47,6 +47,131 @@ const getImages = async (image) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const getEvensByCondition = async (condition) => {
+  const now = new Date();
+
+  const results = await Event.aggregate([
+    {
+      $match: condition,
+    },
+    {
+      $addFields: {
+        lastDate: { $arrayElemAt: ["$dates", -1] },
+      },
+    },
+    {
+      $addFields: {
+        isUpcoming: { $gt: ["$lastDate", now] },
+      },
+    },
+    {
+      $lookup: {
+        from: "items",
+        localField: "venue",
+        foreignField: "_id",
+        as: "venue",
+      },
+    },
+    {
+      $lookup: {
+        from: "items",
+        localField: "catering",
+        foreignField: "_id",
+        as: "catering",
+      },
+    },
+    {
+      $lookup: {
+        from: "items",
+        localField: "decoration",
+        foreignField: "_id",
+        as: "decoration",
+      },
+    },
+    {
+      $lookup: {
+        from: "items",
+        localField: "photograph",
+        foreignField: "_id",
+        as: "photograph",
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        images: 1,
+        ticketPrice: 1,
+        venue: { $arrayElemAt: ["$venue", 0] },
+        catering: { $arrayElemAt: ["$catering", 0] },
+        decoration: { $arrayElemAt: ["$decoration", 0] },
+        photograph: { $arrayElemAt: ["$photograph", 0] },
+        status: 1,
+        rejectedBy: 1,
+        dates: 1,
+        isPublished: 1,
+        isUpcoming: 1,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        upcomingEvents: {
+          $push: {
+            $cond: {
+              if: "$isUpcoming",
+              then: {
+                item: "$$ROOT",
+              },
+              else: "$$REMOVE",
+            },
+          },
+        },
+        pastEvents: {
+          $push: {
+            $cond: {
+              if: { $not: "$isUpcoming" },
+              then: {
+                item: "$$ROOT",
+              },
+              else: "$$REMOVE",
+            },
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        upcomingEvents: 1,
+        pastEvents: 1,
+      },
+    },
+  ]);
+
+  const { upcomingEvents, pastEvents } = results[0] || {
+    upcomingEvents: [],
+    pastEvents: [],
+  };
+
+  // Fetch and assign images for each event in parallel
+  const [upcomingEventsWithImages, pastEventsWithImages] = await Promise.all([
+    Promise.all(
+      upcomingEvents.map(async (event) => ({
+        item: event.item,
+        image: await getImages(event.item.images),
+      })),
+    ),
+    Promise.all(
+      pastEvents.map(async (event) => ({
+        item: event.item,
+        image: await getImages(event.item.images),
+      })),
+    ),
+  ]);
+  return { Upcoming: upcomingEventsWithImages, Past: pastEventsWithImages };
 };
 
 exports.createEvent = catchAsync(async (req, res, next) => {
@@ -110,35 +235,27 @@ exports.createEvent = catchAsync(async (req, res, next) => {
 });
 
 exports.getAllEvents = catchAsync(async (req, res, next) => {
-  const Events = await Event.find({ isPublished: true }).populate(
-    "venue catering decoration photograph",
-  );
-  console.log(Events);
-  let events = [];
-  for (let i = 0; i < Events.length; i++) {
-    let img = await getImages(Events[i].images);
-    events.push({
-      item: Events[i],
-      image: img,
-    });
-  }
-  res.status(200).json({ message: "success", events });
+  const data = await getEvensByCondition({ isPublished: true });
+
+  res.status(200).json({
+    message: "success",
+    Upcoming: data.Upcoming,
+    Past: data.Past,
+  });
 });
 
 exports.getEventsByUserId = catchAsync(async (req, res, next) => {
   const userId = req.params.userId;
-  const Events = await Event.find({ userId }).populate("venue catering decoration photograph");
 
-  let events = [];
-  for (let i = 0; i < Events.length; i++) {
-    let img = await getImages(Events[i].images);
-    events.push({
-      item: Events[i],
-      image: img,
-    });
-  }
-  res.status(200).json({ message: "success", events });
+  const data = await getEvensByCondition({ userId: new mongoose.Types.ObjectId(userId) });
+
+  res.status(200).json({
+    message: "success",
+    Upcoming: data.Upcoming,
+    Past: data.Past,
+  });
 });
+
 exports.getEventsByClientId = catchAsync(async (req, res, next) => {
   const clientId = req.params.clientId;
   const Events = await Booking.find({ clientId }).populate("eventId");
@@ -242,34 +359,25 @@ exports.publishEvent = catchAsync(async (req, res, next) => {
   const eventId = req.params.eventId;
   const event = await Event.findByIdAndUpdate(eventId, { isPublished: true, status: "Confirmed" });
   const userId = event.userId;
-  const Events = await Event.find({ userId }).populate("venue catering decoration photograph");
 
-  let events = [];
-  for (let i = 0; i < Events.length; i++) {
-    let img = await getImages(Events[i].images);
-    events.push({
-      item: Events[i],
-      image: img,
-    });
-  }
+  const data = await getEvensByCondition({ userId: new mongoose.Types.ObjectId(userId) });
 
-  res.status(200).json({ message: "success", events });
+  res.status(200).json({
+    message: "success",
+    Upcoming: data.Upcoming,
+    Past: data.Past,
+  });
 });
 
 exports.cancelEvent = catchAsync(async (req, res, next) => {
   const eventId = req.params.eventId;
   const event = await Event.findByIdAndUpdate(eventId, { isPublished: false, status: "Canceled" });
   const userId = event.userId;
-  const Events = await Event.find({ userId }).populate("venue catering decoration photograph");
+  const data = await getEvensByCondition({ userId: new mongoose.Types.ObjectId(userId) });
 
-  let events = [];
-  for (let i = 0; i < Events.length; i++) {
-    let img = await getImages(Events[i].images);
-    events.push({
-      item: Events[i],
-      image: img,
-    });
-  }
-
-  res.status(200).json({ message: "success", events });
+  res.status(200).json({
+    message: "success",
+    Upcoming: data.Upcoming,
+    Past: data.Past,
+  });
 });
