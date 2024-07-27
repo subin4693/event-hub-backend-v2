d240cf9634ae1504a10e3b50d7d8a382602ebc13
subin4693 <46subin@gmail.com>
2024-07-27 15:26:58 +0530

    origin changed

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "50mb" }));
 const allowedOrigins = [
   "http://localhost:5173",
   "https://event-app-qatar.netlify.app",
-  "https://endearing-cat-2fc9f8.netlify.app",
+  "https://main--endearing-cat-2fc9f8.netlify.app",
 ];
 
 app.use(
