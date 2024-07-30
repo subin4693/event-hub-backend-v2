40037272f32cd48a594bec6a262da3338a8cb736
subin4693 <46subin@gmail.com>
2024-07-30 10:54:00 +0530

    CORS ORIGIN CHANGED

--- app.js
+++ app.js
@@ -25,10 +25,16 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: true, // This allows requests from any origin
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Optional: Include if you need to send cookies or other credentials
   }),
 );
 
